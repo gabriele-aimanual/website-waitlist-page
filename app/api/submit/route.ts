5e73637771f4cfbdf3651166674b1ceb6268083d
Gabriele Sabato <gabriele.sabato@xquire.ai>
2024-07-15 18:44:22 +0200

    Introducing google app script

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -1,32 +1,26 @@
+import { NextResponse } from 'next/server';
+
 export async function POST(request: Request) {
-  const { email } = await request.json();
+    const formData = await request.formData();
+    const email = formData.get('email');
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbwm0rArDzrsp4g84JKAVaXRAaPYPd1Gl6Y6Xio8PrIaPKBOCD7ye0rA6cpWRBUdUCQ-qQ/exec';
 
-  try {
-    const response = await fetch(
-      `https://emailoctopus.com/api/1.6/lists/${process.env.LIST_ID}/contacts`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          api_key: `${process.env.EMAILOCTOPUS_KEY}`,
-          email_address: email,
-        }),
-      }
-    );
-
-    if (response.status === 200) {
-      return new Response("Email submitted successfully!", {
-        status: 200,
-      });
-    } else {
-      return new Response("Failed to submit email!", {
-        status: response.status,
-        statusText: response.statusText,
-      });
+    if (!email) {
+        return NextResponse.json({ error: 'Email is required' }, { status: 400 });
     }
-  } catch (error) {
-    return new Response("Internal server error", { status: 500 });
-  }
+
+    try {
+        const response = await fetch(scriptURL, {
+            method: 'POST',
+            body: new URLSearchParams({ email: email.toString() }),
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        return NextResponse.json({ message: 'Email successfully added to the waiting list!' });
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to submit email' }, { status: 500 });
+    }
 }
