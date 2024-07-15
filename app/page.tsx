831e39cc612048396aa38aa7efebc4bb879405cb
Gabriele Sabato <gabriele.sabato@xquire.ai>
2024-07-15 18:50:31 +0200

    Modified text on the webpage

--- app/page.tsx
+++ app/page.tsx
@@ -19,10 +19,10 @@ export default function Home() {
 
         <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10">
           <h1 className="font-semibold tracking-tight text-zinc-900 text-3xl leading-tight md:text-4xl max-w-lg">
-            Join the conversation: Secure your spot on our Chat App!
+            AIManual
           </h1>
           <p className="text-gray-500">
-            Join the waitlist to be notified when our app is available!
+            AI Solutions for Automated Manual Creation
           </p>
 
           <EmailForm />
