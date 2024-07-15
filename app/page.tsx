0613448913a92eadf6756082d63bb967328996fa
Gabriele Sabato <gabriele.sabato@xquire.ai>
2024-07-15 19:25:46 +0200

    Change background color

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
       <Toaster />
 
       <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="md:h-full h-80 bg-[#FCD0A1] relative overflow-hidden">
+        <div className="md:h-full h-80 bg-[#09255c] relative overflow-hidden">
           <Image
-            src="/mobile-app.png"
-            alt="Mobile App Screenshots"
+            src="/manual.png"
+            alt="Manual Image Dallie"
             fill
             className="object-contain mt-8 md:mt-24 px-14 object-bottom"
           />
