0142f4965fd3d27eaa4a743ca675e8004515b8fe
Gabriele Sabato <gabriele.sabato@xquire.ai>
2024-07-16 13:22:19 +0200

    API not working

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -4,11 +4,13 @@ export async function POST(request: Request) {
     try {
         const formData = await request.formData();
         const email = formData.get('email');
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbwm0rArDzrsp4g84JKAVaXRAaPYPd1Gl6Y6Xio8PrIaPKBOCD7ye0rA6cpWRBUdUCQ-qQ/exec';
 
-        if (!email) {
-            return NextResponse.json({ error: 'Email is required' }, { status: 400 });
+        // Check if email exists and convert it to string
+        if (!email || typeof email !== 'string') {
+            return NextResponse.json({ error: 'Email is required and must be a string' }, { status: 400 });
         }
+
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbwm0rArDzrsp4g84JKAVaXRAaPYPd1Gl6Y6Xio8PrIaPKBOCD7ye0rA6cpWRBUdUCQ-qQ/exec';
 
         const response = await fetch(scriptURL, {
             method: 'POST',
